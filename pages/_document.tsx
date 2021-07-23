49bb20026322a440948f7318174d4891167a1c3a
Paolo Tiu <paolotiu17@gmail.com>
2021-07-23 16:39:06 +0800

    feat: :sparkles: use inter font

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="bg-white dark:bg-black">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+      colors: {
+        subtext: '#A9A8A8',
+      },
+    },
   },
   variants: {
     extend: {},
