0d51c815e7d1dc0200b7c16110c5fc7404066f9a
Paolo Tiu <paolotiu17@gmail.com>
2021-07-24 14:41:21 +0800

    refactor: :recycle: moved landing page to own component

---
+++ components/Landing.tsx
@@ -0,0 +1,55 @@
+import { HIGHLIGHT_COLORS } from '@/utils/constants';
+import { useIsFontReady } from '@/utils/hooks/useIsFontReady';
+import { shuffleArray } from '@/utils/shuffleArray';
+import React, { useEffect, useState } from 'react';
+import { RoughNotationGroup, RoughNotation } from 'react-rough-notation';
+import Button from './Button';
+import LinkTo from './LinkTo';
+import { RainbowHighlight } from './RainbowHighlight';
+
+const Landing = () => {
+  const isFontReady = useIsFontReady();
+  const [colors, setColors] = useState<string[]>([]);
+
+  useEffect(() => {
+    setColors(shuffleArray(HIGHLIGHT_COLORS));
+  }, []);
+  return (
+    <section className="flex items-center justify-center min-h-screen px-4 md:px-24">
+      <RoughNotationGroup show={isFontReady}>
+        <div className="space-y-3 mb-28">
+          <h1 className="heading">
+            Hey, I&apos;m{' '}
+            <span className="whitespace-nowrap">
+              <RoughNotation
+                type="underline"
+                padding={[0, 0, -8, 0]}
+                color={colors[0]}
+                animationDuration={800}
+              >
+                Paolo Tiu
+              </RoughNotation>
+            </span>
+          </h1>
+          <h2 className="heading main text-subtext">
+            I like making cool stuff
+          </h2>
+
+          <p className="max-w-2xl pt-3 md:text-lg">
+            I’m a developer specializing in building products with amazing{' '}
+            <RainbowHighlight color={colors[1]}>performace</RainbowHighlight>,{' '}
+            <RainbowHighlight color={colors[2]}>experiences</RainbowHighlight>,
+            and <RainbowHighlight color={colors[3]}>usability</RainbowHighlight>
+            . I would love if you could{' '}
+            <LinkTo href="/gruset-book">sign my guestbook!</LinkTo>
+          </p>
+          <div>
+            <Button className="mt-5"> Contact Me</Button>
+          </div>
+        </div>
+      </RoughNotationGroup>
+    </section>
+  );
+};
+
+export default Landing;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,15 @@
-import Button from '@/components/Button';
-import LinkTo from '@/components/LinkTo';
+import Landing from '@/components/Landing';
 import Navbar from '@/components/Navbar';
-import { RainbowHighlight } from '@/components/RainbowHighlight';
 import SocialsLine from '@/components/SocialsLine';
-import { HIGHLIGHT_COLORS } from '@/utils/constants';
-import { useIsFontReady } from '@/utils/hooks/useIsFontReady';
-import { shuffleArray } from '@/utils/shuffleArray';
-import React, { useEffect, useState } from 'react';
-import { RoughNotation, RoughNotationGroup } from 'react-rough-notation';
+import React from 'react';
 
 export default function Home() {
-  const isFontReady = useIsFontReady();
-  const [colors, setColors] = useState<string[]>([]);
-
-  useEffect(() => {
-    setColors(shuffleArray(HIGHLIGHT_COLORS));
-  }, []);
   return (
     <div>
       <Navbar />
 
       <main>
-        <section className="flex items-center justify-center min-h-screen px-11 md:px-24">
-          <RoughNotationGroup show={isFontReady}>
-            <div className="space-y-3 mb-28">
-              <h1 className="heading">
-                Hey, I&apos;m{' '}
-                <span className="whitespace-nowrap">
-                  <RoughNotation
-                    type="underline"
-                    padding={[0, 0, -8, 0]}
-                    color={colors[0]}
-                    animationDuration={800}
-                  >
-                    Paolo Tiu
-                  </RoughNotation>
-                </span>
-              </h1>
-              <h2 className="heading main text-subtext">
-                I like making cool stuff
-              </h2>
-
-              <p className="max-w-2xl pt-3 md:text-lg">
-                I’m a developer specializing in building products with amazing{' '}
-                <RainbowHighlight color={colors[1]}>
-                  performace
-                </RainbowHighlight>
-                ,{' '}
-                <RainbowHighlight color={colors[2]}>
-                  experiences
-                </RainbowHighlight>
-                , and{' '}
-                <RainbowHighlight color={colors[3]}>usability</RainbowHighlight>
-                . I would love if you could{' '}
-                <LinkTo href="/gruset-book">sign my guestbook!</LinkTo>
-              </p>
-              <div>
-                <Button className="mt-5"> Contact Me</Button>
-              </div>
-            </div>
-          </RoughNotationGroup>
-        </section>
+        <Landing />
       </main>
       <SocialsLine />
     </div>
