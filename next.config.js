0a174be3678b674bafe2916f3f69defbd0abed26
Paolo Tiu <paolotiu17@gmail.com>
2021-07-31 17:40:28 +0800

    build: :zap: Switch to preact

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-const { withPlausibleProxy } = require('next-plausible');
-
-module.exports = withPlausibleProxy()({
+module.exports = {
   reactStrictMode: true,
-});
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
+};
