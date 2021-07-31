af8e50d1f63c6019b6613823a6c92a4a922824c5
Paolo Tiu <paolotiu17@gmail.com>
2021-07-31 23:09:22 +0800

    refactor: projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,21 +4,47 @@ interface Props {
   imgSrc: string;
   title: string;
   description: string;
+  liveLink: string;
+  repoLink: string;
 }
 
-const ProjectCard = ({ imgSrc, title, description }: Props) => {
+const ProjectCard = ({
+  imgSrc,
+  title,
+  description,
+  liveLink,
+  repoLink,
+}: Props) => {
   return (
-    <div className="w-full cursor-pointer group">
+    <div className="flex flex-col w-full">
       <div
-        className="w-full h-[280px] sm:h-[350px] md:h-[250px] xl:h-[350px] bg-cover rounded bg-center group-hover:transform group-hover:scale-105 transition duration-300"
+        className="w-full h-[280px] sm:h-[350px] md:h-[250px] xl:h-[350px] bg-cover rounded bg-center "
         style={{ backgroundImage: `url(${imgSrc})` }}
       />
       <div className="pt-8">
         <h3 className="text-3xl font-medium">{title}</h3>
-        <p className="pt-5 leading-7 text-subtext">{description}</p>
+        <p className="pt-2 leading-7 text-subtext">{description}</p>
+      </div>
+      <div className="pt-4 mt-auto space-x-4">
+        <OutboundLink href={liveLink}>Live Project</OutboundLink>
+        <OutboundLink href={repoLink}>Github Repo</OutboundLink>
       </div>
     </div>
   );
 };
-
+function OutboundLink({
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<'a'>) {
+  return (
+    <a
+      {...props}
+      className="hover:underline"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      {children}
+    </a>
+  );
+}
 export default ProjectCard;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,16 +8,20 @@ const Projects = () => {
       <SectionTitle subtitle="I like keeping myself busy and having projects to work on. Here are some of the applications I built.">
         Projects
       </SectionTitle>
-      <div className="grid grid-cols-1 gap-x-[140px] gap-y-20 pt-10 lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-x-[140px] gap-y-20 pt-10 lg:grid-cols-2 ">
         <ProjectCard
           imgSrc="/TheCanvasJewelryMockup.png"
           title="The Canvas Jewelry"
-          description="lsdkadklsad"
+          description="An e-commerce website built with Next.JS & Sanity for a jewelry company."
+          liveLink="https://thecanvasjewelry.com"
+          repoLink="https://github.com/paolotiu/canvas-jewelry"
         />
         <ProjectCard
           imgSrc="/DumprMockup.png"
           title="DumpR"
-          description="lsdkadklsad"
+          description="A bookmarking web app with draggable folders and subfolders."
+          liveLink="https://dumpr.vercel.app"
+          repoLink="https://github.com/paolotiu/bookmark-manager"
         />
       </div>
     </section>
