06bac15744a68b6b3fcb23ba6cef4f229dab0027
Paolo Tiu <paolotiu17@gmail.com>
2021-07-31 15:09:03 +0800

    feat: add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import PlausibleProvider from 'next-plausible';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
@@ -10,14 +11,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <ThemeProvider attribute="class">
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <PlausibleProvider
+      domain="paolotiu.com"
+      customDomain="https://plausible.paolotiu.com"
+      selfHosted
+      enabled
+    >
+      <ThemeProvider attribute="class">
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
 export default MyApp;
