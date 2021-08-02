a6f2c3643296a1e9ca6f9e0a8b0a8bbb12ed82c2
Paolo Tiu <paolotiu17@gmail.com>
2021-08-02 16:17:30 +0800

    fix: trigger signature animation only once per session

--- lib/jotai.ts
+++ lib/jotai.ts
@@ -1,3 +1,10 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 
 export const activeHeadingAtom = atom<string | null>(null);
+
+export const animateSignatureAtom = atomWithStorage('animateSignature', true, {
+  getItem: (key) =>
+    JSON.parse(sessionStorage.getItem(key) || 'true') as boolean,
+  setItem: (key, val) => sessionStorage.setItem(key, JSON.stringify(val)),
+});
